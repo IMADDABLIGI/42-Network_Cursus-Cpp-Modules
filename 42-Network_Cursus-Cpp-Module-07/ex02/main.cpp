a34a1d52a9e29fbccb27e44b1c74249ad49faf5a
Imad Dabligi <idabligi@e3r1p17.1337.ma>
2023-10-13 18:17:46 +0100

    03

--- 42-Network_Cursus-Cpp-Module-07/ex02/main.cpp
+++ 42-Network_Cursus-Cpp-Module-07/ex02/main.cpp
@@ -6,38 +6,20 @@
 /*   By: idabligi <idabligi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 11:04:27 by idabligi          #+#    #+#             */
-/*   Updated: 2023/10/12 17:07:57 by idabligi         ###   ########.fr       */
+/*   Updated: 2023/10/13 18:03:01 by idabligi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "iter.hpp"
-
-template <typename A>
-void    printing(A const &X)
-{
-    std::cout << X << std::endl;
-}
-
-void    toUpperPrint(std::string const &X)
-{
-    for(size_t i = 0; i < X.length(); i++)
-    {
-        if (X[i] >= 97 && X[i] <= 122)
-            std::cout << static_cast<char>(X[i] - 32);
-        else
-            std::cout << X[i];
-    }
-}
+#include "Array.hpp"
 
 int main()
 {
-    {
-        int arr[] = {20, 50, 10, 5, 77};
-        iter(arr, 5, printing);
-    }
-    {
-        std::string str[] = {"hello ", ",how are you \n", "C++ is BETTER than C\n"};
-        iter(str, 3, toUpperPrint);
-    }
+    Array<double> A;
+    Array<int> D(0);
+    try
+    { std::cout << A[0]; }
+    catch (std::exception &e)
+    { std::cout << e.what() << std::endl; }
+    std::cout << A.size() << std::endl;
     return (0);
 }
