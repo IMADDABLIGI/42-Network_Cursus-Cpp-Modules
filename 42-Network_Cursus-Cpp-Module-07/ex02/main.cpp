ba928318d0994fd29c93789848d61786a9d34177
Imad Dabligi <imad.dabligi@gmail.com>
2023-10-14 11:19:17 +0100

    Almost Done 033

--- 42-Network_Cursus-Cpp-Module-07/ex02/main.cpp
+++ 42-Network_Cursus-Cpp-Module-07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: idabligi <idabligi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 11:04:27 by idabligi          #+#    #+#             */
-/*   Updated: 2023/10/14 11:08:31 by idabligi         ###   ########.fr       */
+/*   Updated: 2023/10/14 11:19:04 by idabligi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,17 @@ void    ff()
 int main()
 {
     atexit(ff);
-    // {
-    //     Array<double> A;
-    //     Array<int> D(5);
-    //     int *ptr = D.getPtr();
-    //     try
-    //     { std::cout << D[0] << std::endl; }
-    //     catch (std::exception &e)
-    //     { std::cout << e.what() << std::endl; }
-    //     std::cout << D.size() << std::endl;
-    //     delete ptr;
-    // }
+    {
+        Array<double> A;
+        Array<int> D(5);
+        int *ptr = D.getPtr();
+        try
+        { std::cout << D[0] << std::endl; }
+        catch (std::exception &e)
+        { std::cout << e.what() << std::endl; }
+        std::cout << D.size() << std::endl;
+        delete ptr;
+    }
     {
         Array<unsigned int> C(50);
         unsigned int *ptr = C.getPtr(); 
