32f26dabaf9e8c9809a4157604593d1e7f9592f8
Imad Dabligi <imad.dabligi@gmail.com>
2023-12-01 18:20:59 +0100

    Testing Containers

--- 42-Network_Cursus-Cpp-Module-09/test/vec.cpp
+++ 42-Network_Cursus-Cpp-Module-09/test/vec.cpp
@@ -6,7 +6,7 @@
 /*   By: idabligi <idabligi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 16:24:51 by idabligi          #+#    #+#             */
-/*   Updated: 2023/12/01 18:03:09 by idabligi         ###   ########.fr       */
+/*   Updated: 2023/12/01 18:19:39 by idabligi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,4 +43,55 @@ int main()
         for(it = dq.begin(); it != dq.end(); it++)
             std::cout << *it << std::endl;
     }
+     // {
+    //     std::array<int, 5> arr;
+    // arr[0] = 20;
+    // arr[1] = 122;
+    // std::cout << arr[0] << std::endl;
+    // std::cout << arr[1] << std::endl;
+    // std::cout << arr.size() << std::endl;
+    // std::cout << arr.data() << std::endl;
+    // std::cout << "Begin :: " << arr.begin() << std::endl;
+    // std::cout << arr.end() << std::endl;
+    // }
+    // {
+    //     std::vector<int> vec;
+    //     vec.reserve(4);
+    //     std::cout << "size : " << vec.size() << std::endl;
+    //     std::cout << "capacity : " << vec.capacity() << std::endl;
+    //     vec.push_back(50);
+    //     vec.push_back(500);
+    //     vec.push_back(5000);
+    //     vec.push_back(50000);
+    //     vec.push_back(500000);
+    //     std::cout << "size : " << vec.size() << std::endl;
+    //     std::cout << "capacity : " << vec.capacity() << std::endl;
+    //     std::cout << vec[0] << std::endl;
+    //     std::cout << vec[1] << std::endl;
+    //     std::cout << vec.at(2) << std::endl;
+    // }
+    // {
+    //     std::set<int> set; 
+    //     // Inserting elements into the set
+    //     set.insert(2);
+    //     set.insert(5);
+    //     set.insert(3);
+
+    //     std::set<int>::iterator it;
+    //     for (it = set.begin(); it != set.end(); it++)
+    //     {
+    //         std::cout << *it << " ";
+    //     }
+    //     std::cout << std::endl;
+    //     }
+    // {
+    //     std::map<std::string, int> map;
+
+    //     map["age"] = 750;
+    //     map["aae"] = 2165;
+    //     map.insert(std::make_pair("zEDGE", 800));
+    //     std::map<std::string, int>::iterator it;
+    //     for(it = map.begin(); it != map.end(); it++)
+    //         std::cout << it->first << " " << it->second << std::endl;
+    // }
 }
